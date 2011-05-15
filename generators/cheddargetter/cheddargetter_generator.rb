1d83068e8dd36f7d2f3f10b531937d8f521098b7
Brent Wooden <brent.wooden@gmail.com>
2011-05-15 01:12:09 -0400

    Chaging usage and a few things in the generator.

--- generators/cheddargetter/cheddargetter_generator.rb
+++ generators/cheddargetter/cheddargetter_generator.rb
@@ -1,11 +1,11 @@
-class CheddargetterGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.file "cheddargetter_client.rb", "config/initializers/cheddargetter_client.rb"
-    end
-    
-    record do |m|
-      m.file "cheddargetter.yml", "config/cheddargetter.yml"
-    end
+class CheddargetterGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def generate_initializer
+    copy_file "cheddargetter_client.rb", "config/initializers/cheddargetter_client.rb"
+  end
+  
+  def generate_config
+    copy_file "cheddargetter.yml", "config/cheddargetter.yml"
   end
 end
