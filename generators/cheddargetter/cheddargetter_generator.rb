b0c4ee3fd1c2b26a80f553c92351ff65eae6a34e
Brent Wooden <brent.wooden@gmail.com>
2011-05-15 00:51:11 -0400

    Formatting generator file differently.

--- generators/cheddargetter/cheddargetter_generator.rb
+++ generators/cheddargetter/cheddargetter_generator.rb
@@ -1,9 +1,10 @@
 class CheddargetterGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.directory 'config/initializers'
-
-      m.template "cheddargetter_client.rb", "config/initializers/cheddargetter_client.rb"
+      m.file "cheddargetter_client.rb", "config/initializers/cheddargetter_client.rb"
+    end
+    
+    record do |m|
       m.file "cheddargetter.yml", "config/cheddargetter.yml"
     end
   end
